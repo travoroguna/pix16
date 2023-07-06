890588b13884841009998e61fc458014a7e34236
Nick Aversano <nick23hi@gmail.com>
2023-07-05 20:54:03 -0400

    first

---
+++ build.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set project_root=%~dp0%
+set exe_name=pix16_debug.exe
+set build_hash=0
+
+pushd %project_root%
+    FOR /F "tokens=*" %%g IN ('more %project_root%\.git\refs\heads\main') do (set build_hash=%%g)
+
+    if not exist build (mkdir build)
+
+    pushd build
+        set flags=/Od -Oi -Zo -FC -Z7 -fp:fast -fp:except- /Zc:strictStrings- -Gm- -GR- -GS- -Gs9999999
+        cl.exe -nologo /MD -DDEBUG=1 -DBUILD_HASH="%build_hash%" /I ..\src %flags%  ..\src\pix16_win32.cpp /link -subsystem:windows -incremental:no -opt:ref -OUT:%exe_name%
+        IF %errorlevel% NEQ 0 (popd && goto end)
+
+        :: Run
+        .\%exe_name%
+    popd 
+
+popd
---
+++ src/pix16_win32.cpp
@@ -0,0 +1,9 @@
+#define WIN32_LEAN_AND_MEAN
+#define VC_EXTRALEAN
+#define NOMINMAX
+#include <windows.h>
+
+int APIENTRY WinMain(HINSTANCE instance, HINSTANCE prev_inst, LPSTR argv, int argc)
+{
+    return 0;
+}
